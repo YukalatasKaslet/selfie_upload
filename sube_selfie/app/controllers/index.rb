3a279d6248bd1a16d9ec54bc01ae54b5ed7437df
codeamac07 <codeamac07@gmail.com>
2016-08-23 19:08:38 -0500

    se redirige '/' a vista selfie y se añadió ruta para subir imágenes

--- sube_selfie/app/controllers/index.rb
+++ sube_selfie/app/controllers/index.rb
@@ -1,5 +1,14 @@
 get '/' do
   # La siguiente linea hace render de la vista 
   # que esta en app/views/index.erb
-  erb :index
+  @photos = Photo.all
+  erb :selfie
+end
+
+
+post '/upload' do
+  @photo = Photo.new(params[:photo])
+  @photo.save!
+
+  redirect '/'
 end
