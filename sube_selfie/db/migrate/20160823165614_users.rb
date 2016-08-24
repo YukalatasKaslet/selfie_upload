d8ad6a63c8e0b19a42ec0c9ef8f94d425d1f11b5
codeamac07 <codeamac07@gmail.com>
2016-08-23 19:09:09 -0500

    se añadió a la migración users tabla photos

--- sube_selfie/db/migrate/20160823165614_users.rb
+++ sube_selfie/db/migrate/20160823165614_users.rb
@@ -6,7 +6,13 @@ class Users < ActiveRecord::Migration
       #has_secure_password
       #Adds methods to set and authenticate against a BCrypt password.
       #This mechanism requires you to hace a << password_digest >> attribute
-      t.string    :password_digest, nul: false
-       
+      t.string    :password_digest, nul: false 
+      t.timestamps 
     end
+
+    create_table :photos do |t|
+      t.string  :photo
+      t.timestamps 
+    end     
+  end
 end
